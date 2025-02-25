ed13ffabed792cdfad75a8b785bb28c3b060018c
Mahesh-Banduni <maheshbanduni9997@gmail.com>
2025-02-25 13:35:40 +0530

    added code

--- frontend/src/pages/OlaMap.jsx
+++ frontend/src/pages/OlaMap.jsx
@@ -5,7 +5,7 @@ const OlaMap = () => {
     useEffect(() => {
         // Initialize Ola Maps with API Key
         const olaMaps = new OlaMaps({
-            apiKey: "WONLJdKUeBe70Chbm9Dgr7wPNDlaXbIR6izkx25d", // Replace with your API key
+            apiKey: `${import.meta.env.VITE_OLAMAPS_API_KEY}`, // Replace with your API key
         });
 
         // Render the map
